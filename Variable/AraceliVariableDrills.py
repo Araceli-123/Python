503942d08d988793c3048db884f3669a9cc99152
Araceli-123 <73916752+Araceli-123@users.noreply.github.com>
2020-11-11 12:32:13 -0600

    11/11/20

---
+++ Variable/AraceliVariableDrills.py
@@ -0,0 +1,88 @@
+'''Integers'''
+
+var1 = 5
+print(var1)
+
+var2 = 10
+print(var2)
+
+var3 = var2
+print(var3)
+
+x = 24
+print(x)
+
+y = 100
+print(y) 
+
+'''Floats'''
+
+z = 1.1
+print(z) 
+
+a = 2.75
+print(a)
+
+b = a
+print(b)
+
+c = 100.1
+print(c)
+
+d = 12.3
+print(d)
+
+"Strings"
+
+name = "Araceli"
+print(name)
+
+e = (name[0])
+print(e)
+
+f = (name[6])
+print(f)
+
+g = "chicken"
+print(g)
+
+h = "Ara"
+print(h)
+
+'''Booleans'''
+
+done = False 
+print(done)
+
+smart = True
+print(smart)
+
+i = done
+print(i)
+
+j = False 
+print(j) 
+
+almostDone = True
+print(almostDone)
+
+'''Lists'''
+
+listInt = [1, 2, 3, 4, 5]
+
+print(listInt[2])
+
+listString = ["a","b","c","d","e"]
+
+print(listString[3])
+
+listBoolean = [True, False, False, True, True] 
+print(listBoolean[0])
+print(listBoolean[1])
+print(listBoolean[2])
+print(listBoolean[3])
+print(listBoolean[4])
+
+
+
+
