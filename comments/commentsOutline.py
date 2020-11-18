f9650e4e3dc30e0663b79d467190c9d9ff253e5c
Araceli-123 <73916752+Araceli-123@users.noreply.github.com>
2020-11-18 16:00:05 -0600

    Add files via upload

---
+++ comments/commentsOutline.py
@@ -0,0 +1,22 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#hello there
+
+#2) Make a multi-line comment.
+
+'''
+hello
+howdy
+hola
+
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+a=8 #one a equals 8 
