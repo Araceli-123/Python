9ed727fe7b7c50cc39ba668db4568b89b44c1e12
Araceli-123 <73916752+Araceli-123@users.noreply.github.com>
2020-11-18 15:57:02 -0600

    11/18/20

---
+++ comments/commentDrills.py
@@ -0,0 +1,39 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+# How was your day?
+
+#2) Make a multi-line comment.
+
+'''All I did today
+was do homework'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+h = 123 # as stated h equals one hundred and twenty three.
+
+#4) Make a single line comment.
+
+#hope you have a great day 
+
+
+#5) Make a multi-line comment.
+
+''' I like 
+soccer'''
