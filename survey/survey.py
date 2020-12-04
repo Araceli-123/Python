98a87fc29a762b315c45d1bbcd758c8d40f95e7b
Araceli-123 <73916752+Araceli-123@users.noreply.github.com>
2020-12-04 17:48:51 -0600

    12/4/2020

---
+++ survey/survey.py
@@ -0,0 +1,18 @@
+'''
+This program is meant to take survey information.
+@author: Araceli Negrete-Garcia
+'''
+name = input("What is your name?")
+print(name)
+
+color = input("What is your favorite color?")
+print(color)
+
+city = input("What city did you grow up in?")
+print(city)
+
+friend = input("What is your best friends name?")
+print(friend)
+
+hair = input("What color hair do you have?")
+print(hair)
